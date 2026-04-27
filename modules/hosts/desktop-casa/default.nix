fcd08c1ecdfea4def31ba3c9baea86949f9440cb
Arthaaas <iarthu0@gmail.com>
2026-04-27 15:51:53 -0300

    fix: boot desktop with linux 6.12 and matching nvidia driver

--- modules/hosts/desktop-casa/default.nix
+++ modules/hosts/desktop-casa/default.nix
@@ -1,9 +1,12 @@
 { self, inputs, ... }: {
-    flake.nixosConfigurations.desktopCasa = inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs self; };
-        modules = [
-            self.nixosModules.desktopCasaConfiguration
-            inputs.home-manager.nixosModules.home-manager
-        ];
-    };
+  flake.nixosConfigurations.desktopCasa = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs self; };
+    modules = [
+      self.nixosModules.desktopCasaConfiguration
+      inputs.home-manager.nixosModules.home-manager
+      ({ pkgs, ... }: {
+        programs.niri.package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+      })
+    ];
+  };
 }
